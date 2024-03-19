8f495e9dd3741bb0ec2d02e30fb8d031dbd125a9
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-03-19 12:17:12 +0200

    created a file for color pallete and home page

---
+++ lib/constansts/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color darkGrayColor = Color(0xFF242526);
+const Color navyBlueColor = Color(0xFF014268);
+const Color powderBlueColor = Color(0xFF97B5C6);
+const Color royalBlueColor = Color(0xFF3975C4);
